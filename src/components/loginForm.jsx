75efb54ac12ffba2686565f2b1a42e82396ff4e8
RachitMalik12 <rachitmalik1947@gmail.com>
2019-05-28 18:46:59 +0530

    added loginService to login users and added error handling

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import { login } from '../services/authService';
+
+
 
 class LoginForm extends Form {
   state = {
@@ -17,10 +20,25 @@ class LoginForm extends Form {
       .label("Password")
   };
 
-  doSubmit = () => {
+  doSubmit = async () => {
     // Call the server
     console.log("Submitted");
+    try {
+      const {username, password} = this.state.data; 
+      await login(username,password ); 
+      
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400)
+      {
+        const errors = {...this.state.errors};
+        errors.username = ex.response.data; 
+        this.setState({errors}); 
+      }
+      
+    }
   };
+
+
 
   render() {
     return (
