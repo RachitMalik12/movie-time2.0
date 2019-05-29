42a35983cf6a18c1f1766ab8b63106a9ab8598e9
RachitMalik12 <rachitmalik1947@gmail.com>
2019-05-29 12:07:55 +0530

    Cleaned up code

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -21,19 +21,28 @@ class MoviesTable extends Component {
         <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
       )
     },
-    {
-      key: "delete",
-      content: movie => (
-      
-       auth.getCurrentUser() && auth.getCurrentUser().isAdmin && <button
-          onClick={() => this.props.onDelete(movie)}
-          className="btn btn-danger btn-sm"
-        >
-          Delete
-        </button>
-      )
-    }
+    
   ];
+
+  deleteCol = {
+    key: "delete",
+    content: movie => (
+    
+     auth.getCurrentUser() && auth.getCurrentUser().isAdmin && <button
+        onClick={() => this.props.onDelete(movie)}
+        className="btn btn-danger btn-sm"
+      >
+        Delete
+      </button>
+    )
+  };
+
+  constructor(){
+    super(); 
+    const user = auth.getCurrentUser();
+    if (user && user.isAdmin) this.columns.push(this.deleteCol); 
+    
+  }
 
   render() {
     const { movies, onSort, sortColumn } = this.props;
